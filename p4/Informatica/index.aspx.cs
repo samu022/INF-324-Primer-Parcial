0cccd6106bb87d4bf6dadf6a8f8a2e8073b68118
samu022 <95974151+samu022@users.noreply.github.com>
2023-04-12 19:13:32 -0400

    Add files via upload

---
+++ p4/Informatica/index.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace p2.Informatica
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["usuariologueado"] != null)
+            {
+                string usuariologueado = Session["usuariologueado"].ToString();
+                lblBienvenida.Text = "Bienvenido/a " + usuariologueado;
+                if (Session["rol"] != null && Session["rol"].ToString() == "director")
+                {
+                    // Mostrar el botón para administradores
+                    BtnDirector.Visible = true;
+                }
+                else
+                {
+                    // Ocultar el botón para no-administradores
+                    BtnDirector.Visible = false;
+                }
+            }
+            else
+            {
+                Response.Redirect("../login.aspx");
+            }
+        }
+        protected void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Session.Remove("usuariologueado");
+            Response.Redirect("../login.aspx");
+        }
+    }
+}
